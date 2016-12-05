99a7ff7ae5e2380a15165deb74f393a0693d5a0d
massindia <mr.perfect@live.co.uk>
2016-12-05 18:47:21 +0530

    Ascii print Program
    Print Ascii code from A to Z

---
+++ src/asciicodeprint.java
@@ -0,0 +1,21 @@
+
+public class asciicodeprint {
+	public static void main(String[] args) {
+		
+		char A ='A', B='Z';
+		
+		for(int n=A; n<=B; n++,A++)
+		{
+		System.out.println("ASCII Value is "+n+"-"+A+"");
+
+		
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
